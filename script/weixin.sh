8af8bfbe21ab36e17d83974daa1ec47663881136
root <root@iZm5e2u21foywgwaq0v9g7Z.(none)>
2017-07-13 22:45:13 +0800

    by wuchengyi

---
+++ script/weixin.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+python_env='/home/wcy/env/bin/activate'
+dir='/home/wcy/myspider'
+
+if [ $# -eq 1 ];then
+    date=`date -d "$1" +%Y%m%d`
+elif [ $# -eq 0 ];then
+    date=`date -d '-1 day' +%Y%m%d`
+else
+    echo '[ERROR] Wrong params number!'
+    echo '[USAGE] $0 date'
+    exit
+fi
+
+echo $date
+
+source $python_env
+cd $dir
+scrapy crawl weixin -a date=$date -o $dir/result/weixin/$date.json -t json
